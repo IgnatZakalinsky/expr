fe4ad59f254a0303958f56f92cb6e23ee9b75ae6
neko <ai73a@yandex.ru>
2022-06-06 19:36:48 +0300

    check errors

--- src/video/video.ts
+++ src/video/video.ts
@@ -9,6 +9,11 @@ export const getVideos = (req: Request, res: Response) => {
     res.status(200).json(videos)
 }
 export const addVideo = (req: Request, res: Response) => {
+    if (typeof req.body.title !== 'string') {
+        res.status(400).json({errorsMessages: [{message: 'not string', field: "title"}], resultCode: 1})
+        return
+    }
+
     const newVideo = {
         id: Date.now(),
         title: req.body.title,
@@ -21,10 +26,20 @@ export const getVideo = (req: Request, res: Response) => {
     res.status(200).json(videos.find(v => v.id === +req.params.id))
 }
 export const delVideo = (req: Request, res: Response) => {
-    videos = videos.filter(v => v.id !== +req.params.id)
-    res.status(204).json({})
+    const x = videos.find(v => v.id === +req.params.id)
+    if (x) {
+        videos = videos.filter(v => v.id !== +req.params.id)
+        res.status(204).json({})
+    } else {
+        res.status(404).json({})
+    }
 }
 export const changeVideo = (req: Request, res: Response) => {
+    if (typeof req.body.title !== 'string') {
+        res.status(400).json({errorsMessages: [{message: 'not string', field: "title"}], resultCode: 1})
+        return
+    }
+    
     videos = videos.map(v => v.id === +req.params.id ? {...v, title: req.body.title} : v)
     res.status(204).json({})
 }
