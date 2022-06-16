fb65031da51551ab1021c5f788e20a833c02ec5a
neko <ai73a@yandex.ru>
2022-06-16 18:05:13 +0300

    fix, check ip

--- src/s2-hw1/blogger/blogersAndPostsRouter.ts
+++ src/s2-hw1/blogger/blogersAndPostsRouter.ts
@@ -6,8 +6,11 @@ import {body, validationResult} from 'express-validator'
 export const validationsErrorsMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        res.status(400).json({ errors: errors.array() })
+        return
     }
+
+    next()
 }
 
 export const bloggersRouter = Router()
--- src/s2-hw1/hw1.ts
+++ src/s2-hw1/hw1.ts
@@ -4,6 +4,36 @@ import {bloggersRouter, postsRouter} from './blogger/blogersAndPostsRouter'
 
 const blackIPs = ['1']
 const blockIPsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    
+    console.log(req.headers['x-forwarded-for'])
+    console.log(req.headers['X-Forwarded-For'])
+    console.log(req.headers['x-client-ip'])
+    console.log(req.headers['X-Client-IP'])
+    console.log(req.headers['CF-Connecting-IP'])
+    console.log(req.headers['cf-connecting-ip'])
+    console.log(req.headers['Fastly-Client-Ip'])
+    console.log(req.headers['Fastly-Client-IP'])
+    console.log(req.headers['fastly-client-ip'])
+    console.log(req.headers['True-Client-Ip'])
+    console.log(req.headers['True-Client-IP'])
+    console.log(req.headers['true-client-ip'])
+    console.log(req.headers['X-Real-IP'])
+    console.log(req.headers['x-real-ip'])
+    console.log(req.headers['X-Cluster-Client-IP'])
+    console.log(req.headers['x-cluster-client-ip'])
+    console.log(req.headers['X-Forwarded'])
+    console.log(req.headers['x-forwarded'])
+    console.log(req.headers['Forwarded-For'])
+    console.log(req.headers['forwarded-for'])
+    console.log(req.headers['Forwarded'])
+    console.log(req.headers['forwarded'])
+    console.log(req.connection?.remoteAddress)
+    // @ts-ignore
+    console.log(req.connection?.socket?.remoteAddress)
+    console.log(req.socket?.remoteAddress)
+    // @ts-ignore
+    console.log(req.info?.remoteAddress)
+
     if (blackIPs.find(i => i === req.query.ip)) {
         res.status(403).json({reason: 'blackIP'})
         return
@@ -20,7 +50,7 @@ const countMiddleware = (req: Request, res: Response, next: NextFunction) => {
 }
 
 const checkContentTypeMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.query.contentType !== 'application/json') {
+    if (req.headers['content-type'] !== 'application/json') {
         res.status(400).json({error: 'Bad content type'})
         return
     }
